9c2e028f4510329e668317aa7874e4a6e2d913a0
mukeshabdare <mail.abdare@gmail.com>
2020-08-29 21:00:31 +0530

    First commit

---
+++ java-8-features/src/examples/stream/api/StreamsAPI.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package examples.stream.api;
+
+/**
+ * @author abdare
+ *
+ */
+public class StreamsAPI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Lets start");
+	}
+
+}
